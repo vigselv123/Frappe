b274e16193b2563e5ce676e572ab7cc8ac27d539
Bhuvanesh Selvaraj <bhuvanesh.sel@gmail.com>
2024-10-02 14:06:52 -0700

    Library Member changes

--- library_management/library_management/doctype/library_member/library_member.py
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -9,4 +9,5 @@ from frappe.model.document import Document
 class LibraryMember(Document):
 	def before_save(self):
 		self.full_name = f'{self.first_name} {self.last_name or ""}'
+		self.middle_name = "John"
 
