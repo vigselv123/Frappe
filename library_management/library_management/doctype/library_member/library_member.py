f9866da600cafc6befe4944359aae474c1914755
Shruti Gupta <shrutiguptait001@gmail.com>
2024-10-04 11:55:55 +0000

    added comment

--- library_management/library_management/doctype/library_member/library_member.py
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -8,3 +8,5 @@ from frappe.model.document import Document
 class LibraryMember(Document):
 	def before_save(self):
 		self.full_name = f'{self.first_name} {self.last_name or ""}'
+
+		# self.name="temp1"
